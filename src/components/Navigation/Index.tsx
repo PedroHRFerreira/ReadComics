de658d9c61e6087f7d2d4c2d6afdf41171eb1a5c
PedroHRFerreira <devpedrohr@gmail.com>
2025-09-07 15:58:31 -0300

    feat: navegação

--- src/components/Navigation/Index.tsx
+++ src/components/Navigation/Index.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 import style from "./style.module.scss";
+import Discover from "../Discover/Index";
 
 const Navigation = () => {
-  return <nav className={style.navigation}>teste</nav>;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const pages = [
+    {
+      name: "pages",
+      component: Discover,
+    },
+    {
+      name: "comments",
+      component: Discover,
+    },
+    {
+      name: "users",
+      component: Discover,
+    },
+  ];
+  const ActiveComponent = pages[activeIndex].component;
+  return (
+    <nav className={style.navigation}>
+      <section className={style.navigation__content}>
+        {pages.map((text, index) => (
+          <div
+            key={index}
+            style={{ cursor: "pointer", opacity: 1 }}
+            className={
+              index === activeIndex ? style.navigation__content__active : ""
+            }
+            onClick={() => setActiveIndex(index)}
+          >
+            <h1>{text.name}</h1>
+          </div>
+        ))}
+      </section>
+      <section className={style.navigation__content__component}>
+        <ActiveComponent />
+      </section>
+    </nav>
+  );
 };
 
 export default Navigation;
